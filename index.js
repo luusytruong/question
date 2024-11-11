3e4b00c5889875f62f0e6527009d54d88ee7e5fe
luusytruong <lst27062004@gmail.com>
2024-11-12 04:41:37 +0700

    finish choose answer, play sound effect

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+//func sound eff
+function sound(type) {
+  if (type == "correct") {
+    const sound = new Audio("./sound/correct.mp3");
+    sound.currentTime = 0;
+    sound.play();
+  } else if (type == "uwu") {
+    const sound = new Audio("./sound/uwu.mp3");
+    sound.currentTime = 0.7;
+    sound.play();
+  }
+}
 //anti spam
 let cooldown = true;
 //func return height for question
@@ -14,39 +26,173 @@ function listenerAnswerClick() {
   const answers = document.querySelectorAll(".answer");
   answers.forEach((answer) => {
     answer.addEventListener("click", () => {
+      const parent = answer.closest(".question");
+      if (parent.classList.contains("answered")) {
+        console.log("da tl r");
+        return;
+      }
       if (!cooldown) {
         return;
       }
       cooldown = false;
-      const parent = answer.closest(".question");
+      const correctAnswer = parent
+        .querySelector(".correct-answer")
+        .innerText.trim()
+        .toString();
+      const currentAnswer = answer
+        .querySelector(".answer-body p")
+        .innerText.trim()
+        .toString();
+      console.log(currentAnswer);
+
+      if (currentAnswer === correctAnswer) {
+        answer.classList.add("correct");
+        sound("correct");
+      } else {
+        answer.classList.add("wrong");
+        sound("uwu");
+      }
+      parent.classList.add("answered");
       parent.classList.add("animated");
       setTimeout(() => {
         parent.classList.remove("animated");
         cooldown = true;
-        // scrollQuestion(parent);
-      }, 1000);
-      console.log("hah");
+        scrollQuestion(parent);
+      }, 1500);
     });
   });
 }
-listenerAnswerClick();
 
 //process file from field input
 
-document.getElementById("file").addEventListener("change", function(e) {
+document.getElementById("file").addEventListener("change", function (e) {
   const files = e.target.files;
   const file = files[files.length - 1];
   const fileName = this.closest(".wrapper").querySelector(".file-upload-name");
 
   if (file) {
     const reader = new FileReader();
-
     reader.onload = (event) => {
       fileName.textContent = file.name;
-      document.getElementById('start').classList.add('enable')
+      localStorage.setItem("fileName", JSON.stringify(file.name));
       console.log("he", file.name);
+      try {
+        const jsonData = JSON.parse(event.target.result);
+        if (jsonData.questions) {
+          console.log(jsonData);
+          localStorage.setItem("questions", JSON.stringify(jsonData.questions));
+          loadQuestions();
+          console.log("da luu");
+        } else {
+          console.error("json khong dung yeu cau");
+        }
+      } catch (error) {
+        console.error("loi khi phan tich json", error);
+      }
     };
-
-    reader.readAsDataURL(file);
+    reader.readAsText(file);
   }
 });
+
+//func check json data from local
+function checkData() {
+  const jsonData = JSON.parse(localStorage.getItem("questions"));
+  const jsonName = JSON.parse(localStorage.getItem("fileName"));
+  let found = false;
+  if (jsonData && jsonName) {
+    console.log("co data", jsonData, jsonName);
+    document.getElementById("start").classList.add("enable");
+    document.querySelector(".file-upload-name").textContent = jsonName;
+    listenerBtnStart();
+    listenerBtnRemove();
+    found = true;
+  } else {
+    console.log("kco data");
+    document.getElementById("start").classList.remove("enable");
+  }
+  return found;
+}
+//func listenner click start button
+function listenerBtnStart() {
+  document
+    .querySelector("button.enable")
+    .addEventListener("click", function () {
+      console.log(this);
+      document.querySelector(".json-input").classList.remove("active");
+      document.querySelector(".overlay").classList.remove("active");
+    });
+}
+//func listenner click remove button
+function listenerBtnRemove() {
+  document.getElementById("remove").addEventListener("click", () => {
+    localStorage.removeItem("questions");
+    localStorage.removeItem("fileName");
+    window.location.href = "./";
+  });
+}
+//func create ele question
+function createElemQuestion(index, question, correct, options) {
+  const optionElem = options
+    .map(
+      (option, index) =>
+        `
+        <li class="answer">
+            <div class="answer-head">
+                ${String.fromCharCode(65 + index)}
+            </div>
+            <div class="answer-body">
+                <p>
+                    ${option}
+                </p>
+            </div>
+        </li>
+    `
+    )
+    .join(" ");
+  const html = `
+        <div class="question-head">
+            <div class="question-num">
+                <strong>#<span>${index + 1}</span></strong>
+            </div>
+            <div class="question-content">
+                <p>
+                    ${question}
+                </p>
+            </div>
+            <div class="correct-answer">
+                <p>
+                    ${correct}
+                </p>
+            </div>
+        </div>
+        <ul class="question-body">
+            ${optionElem}
+        </ul>
+    `;
+  const questionElem = document.createElement("div");
+  questionElem.className = "question";
+  questionElem.innerHTML = html;
+  return questionElem;
+}
+//func load question
+function loadQuestions() {
+  if (checkData()) {
+    const jsonDatas = JSON.parse(localStorage.getItem("questions"));
+    document.querySelector(".question-list").innerHTML = "";
+    jsonDatas.forEach((data, index) => {
+      console.log(data);
+      const questionElem = createElemQuestion(
+        index,
+        data.question,
+        data.correct_answer,
+        data.options
+      );
+      document.querySelector(".question-list").appendChild(questionElem);
+    });
+    listenerAnswerClick();
+    // console.log(jsonDatas[0].options[0]);
+  } else {
+    console.log("kh co data de load");
+  }
+}
+loadQuestions();
